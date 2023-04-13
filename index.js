e7f4f9f61720177f0898343f0f487eefa3cb7792
Denn05 <dennis.damalerio05@gmail.com>
2023-04-14 00:43:42 +0800

    for 600px

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ var typed = new Typed(".multilple-text",{
     backDelay: 1000,
     loop: true
 })
+
+var sidemenu = document.getElementById('sidemenu');
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
