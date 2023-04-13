5d81a812a3e0252d5a2a059275a5991729fba7ea
Denn05 <dennis.damalerio05@gmail.com>
2023-04-13 23:02:05 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multilple-text",{
+    strings: ["a Virtual Assistant","an Executive Assistant", "an Ecommerce VA","a Social Media Manager"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
