617f1b2dd8e1cf258a8324eb5c17c921dcfeff93
Denn05 <dennis.damalerio05@gmail.com>
2023-04-15 04:16:22 +0800

    template done

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-var typed = new Typed(".multilple-text",{
-    strings: ["a Virtual Assistant","an Executive Assistant", "an Ecommerce VA","a Social Media Manager"],
-    typeSpeed: 50,
-    backSpeed: 50,
+var typed = new Typed(".multiple-text", {
+    strings: ["a Virtual Assistant", "an Executive Assistant", "an Ecommerce VA", "a Social Media Manager"],
+    typeSpeed: 70,
+    backSpeed: 70,
     backDelay: 1000,
     loop: true
 })
 
-var sidemenu = document.getElementById('sidemenu');
 
-function openmenu(){
-    sidemenu.style.right = "0";
-}
-function closemenu(){
-    sidemenu.style.right = "-200px";
-}
+// TOGGLE ICON BAR
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+// SCROLL SECTION ACTIVE LINK
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+var typed = new typed(".mutiple-text", {
+    strings: ["a Virtual Assistant", "an Executive Assistant", "an Ecommerce VA", "a Social Media Manager"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+// ScrollReveal({ 
+//     reset: true,
+//     distance: '80px',
+//     duration: 2000,
+//     delay: 200 
+// });
+
+// ScrollReveal().reveal('.home-content, .heading', { origin:'top'});
