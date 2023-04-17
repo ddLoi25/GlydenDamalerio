9dd2e3597486e9b4ab9e4259a702e37fac9a6d26
Denn05 <dennis.damalerio05@gmail.com>
2023-04-17 18:11:26 +0800

    fix contact form addeventlistener

--- index.js
+++ index.js
@@ -38,40 +38,44 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+// Get the form element
+const contactForm = document.querySelector('.form-contact');
 
-function sendMail(){
-    // Get the form element
-    var form = document.querySelector('.form-contact');
+// Add a submit event listener to the form
+contactForm.addEventListener('submit', function(event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
 
-    // Check if the form is valid
-    if (form.checkValidity()) {
-        // Get the form field values
-        var params = {
-            name: document.getElementById('fname').value,
-            email: document.getElementById('email').value,
-            mobile: document.getElementById('mobile').value,
-            subject: document.getElementById('subject').value,
-            message: document.getElementById('message').value,
-        };
+  // Check if the form is valid
+  if (contactForm.checkValidity()) {
+    // Get the form field values
+    const params = {
+      name: document.getElementById('fname').value,
+      email: document.getElementById('email').value,
+      mobile: document.getElementById('mobile').value,
+      subject: document.getElementById('subject').value,
+      message: document.getElementById('message').value,
+    };
 
-        // Send the email
-        const serviceID = "service_8op2hkl";
-        const templateID = "template_v8dwu6i";
+    // Send the email
+    const serviceID = "service_8op2hkl";
+    const templateID = "template_v8dwu6i";
 
-        emailjs.send(serviceID, templateID, params)
-        .then(
-            res =>{
-                document.getElementById("fname").value = "";
-                document.getElementById("email").value = "";
-                document.getElementById("mobile").value = "";
-                document.getElementById("subject").value = "";
-                document.getElementById("message").value = "";
-                console.log(res);
-                alert("Your message sent successfully!");
-            })
-        .catch((err) => console.log(err));
-    } else {
-        // Display an error message to the user
-        alert("Please fill out all the required fields.");
-    }
-};
+    emailjs.send(serviceID, templateID, params)
+    .then(
+      res => {
+        document.getElementById("fname").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("mobile").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!");
+      })
+    .catch(err => console.log(err));
+  } else {
+    // Display an error message to the user
+    alert("Please fill out all the required fields.");
+  }
+});
+
